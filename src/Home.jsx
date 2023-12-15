e9379e01745d6d2b0a65fb1ca6cd7c20b1c266a7
Alex Hewitt <alexhewitt.j@gmail.com>
2023-12-15 14:16:39 -0600

    add basic functionality for blog lists

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React from "react"
+import { useState } from 'react'
+import BlogList from './BlogList'
 
 function Home() {
-    const handleClick = () => {
-        console.log("hello")
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'New website', author: 'Alex', id: 1},
+        {title: 'Welcome party!', body: 'Hello there', author: 'mario', id: 2},
+        {title: 'Web dev tips', body: 'Welcome!', author: 'Alex', id: 3}
+    ])
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
     }
+
     return (
         <div className="home">
-            <h2>Home Page</h2>
-            <button onClick={handleClick}>Click me</button>
+            <BlogList
+                blogs={blogs}
+                title="All Blogs!"
+                handleDelete={handleDelete}
+            />
+            <BlogList
+                blogs={blogs.filter(blog => blog.author === 'mario')}
+                title="Mario's Blogs"
+                handleDelete={handleDelete}
+            />
         </div>
     )
 }
